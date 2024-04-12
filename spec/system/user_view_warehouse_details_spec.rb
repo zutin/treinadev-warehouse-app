b2fc2a1da618bb4348a814e5d8627b44c66e4f2f
zutin <gianluccaz@hotmail.com>
2024-04-12 08:33:46 -0300

    User can check warehouse details and go back to home page

--- spec/system/user_view_warehouse_details_spec.rb
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -8,7 +8,7 @@ describe 'User checks warehouse details' do
                       description: 'This warehouse is designed to receive international freight.')
 
     #Act
-    visit('/')
+    visit(root_path)
     click_on('São Paulo')
 
     #Assert
@@ -18,5 +18,20 @@ describe 'User checks warehouse details' do
     expect(page).to have_content('Area: 100000 m²')
     expect(page).to have_content('Address: Avenida do Aeroporto, 1000 Zip Code: 15000-000')
     expect(page).to have_content('This warehouse is designed to receive international freight.')
+  end
+
+  it 'can go back to home page' do
+    #Arrange
+    Warehouse.create(name: 'São Paulo', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                      address: 'Avenida do Aeroporto, 1000', zipcode: '15000-000',
+                      description: 'This warehouse is designed to receive international freight.')
+
+    #Act
+    visit(root_path)
+    click_on('São Paulo')
+    click_on('Back')
+
+    #Assert
+    expect(current_path).to eq(root_path)
   end
 end
